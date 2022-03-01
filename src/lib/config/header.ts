01910b84413deaeb2de15decfd0f2025c53bc644
Monologue.№ <cancan@usc.edu>
2022-03-01 13:56:38 -0800

    build again

--- src/lib/config/header.ts
+++ src/lib/config/header.ts
@@ -4,8 +4,9 @@ interface headerConfig {
 }
 
 export const config: headerConfig = {
-  // nav: {
-  //   '/hello-world': 'The Great Gatsby',
-  //   '/hello-world/elements': 'Test'
-  // }
+  nav: {
+    '/hello-world/prose11': '露水小段',
+    '/hello-world/prose49': '问月集',
+    '/hello-world/prose51': '浅草集'
+  }
 }
